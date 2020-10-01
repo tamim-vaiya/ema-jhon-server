60f1e748588482120297da994c0615f950c8c02e
tamimvaiya <tamimvaiya.123@gmail.com>
2020-10-01 13:36:45 +0600

    done

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(cors());
 
 const port = 5000;
 
+app.get('/', (req, res) => {
+  res.send('Hello from db, it is working');
+})
+
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
